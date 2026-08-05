7192ecb208a7a5db155a6217aee2ba523b2cdc7c
taufiq72 <taufiqq1207@gmail.com>
2026-08-05 10:29:14 +0700

    menambahkan komik.js

---
+++ models/komik.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sinopsis: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        penulis_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: 'komik',
+        timestamps: false
+    });
+    Komik.associate = (models) => {
+        Komik.belongsTo(models.Penulis, {
+            foreignKey: 'penulis_id',
+            as: 'penulis'
+        });
+        Komik.belongsToMany(models.Genre, {
+            through: 'komik_genres',
+            foreignKey: 'komik_id',
+            otherKey: 'genre_id',
+            as: 'genre'
+        });
+    };
+    return Komik;
+};
